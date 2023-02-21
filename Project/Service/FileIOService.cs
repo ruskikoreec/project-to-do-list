4e7b428c27af8b21c129014f683ca94d85de6be6
ruskikoreec <113416751+ruskikoreec@users.noreply.github.com>
2023-02-21 10:06:40 +0300

    SaveDataComplete

--- Project/Service/FileIOService.cs
+++ Project/Service/FileIOService.cs
@@ -5,19 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Project.Service
 {
     class FileIOService
     {
-        private readonly string _filePath;
+        private readonly string FilePath;
+        public FileIOService(string filePath)
+        {
+            FilePath = filePath;
+        }
         public BindingList<ToDoModel> LoadData()
         {
             return null;
         }
         public void SaveData(BindingList<ToDoModel> todoDataList)
         {
-
+            using(StreamWriter writer = File.CreateText(FilePath))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
         }
 
     }
