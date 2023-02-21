c43f11d086d5f175198bcf97e84c0d568b20093f
polinak103 <113394015+polinak103@users.noreply.github.com>
2023-02-21 03:01:28 +0300

    Сохранение изменений
    +событие propertyChange

--- Project/Models/ToDoModel.cs
+++ Project/Models/ToDoModel.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project.Models
 {
-    internal class ToDoModel
+    internal class ToDoModel : INotifyPropertyChanged
+
     {
         public DateTime CreationDate { get; set; } = DateTime.Now;
         private string _text;
         private bool _isDone;
+
+
         public bool IsDone
         {
             get { return _isDone; } 
-            set { _isDone = value; }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged();
+            }
         }
-        public string Text 
-        { 
-            get { return _text; } 
-            set { _text = value; } 
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName ="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
