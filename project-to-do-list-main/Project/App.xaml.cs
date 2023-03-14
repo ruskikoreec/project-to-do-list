0497b7c2287988e710d49e7d8f1d1f53ddbe191d
Sonya3008 <s.vizgirda01@gmail.com>
2023-03-14 12:14:56 +0300

    даты
    Поменяла даты на более привычные нам.

---
+++ project-to-do-list-main/Project/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Markup;
+
+namespace Project
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var russianCultureInfo = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentCulture = russianCultureInfo;
+            Thread.CurrentThread.CurrentUICulture = russianCultureInfo;
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
+    }
+}
